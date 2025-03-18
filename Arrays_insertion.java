c1a006320c8a917c3c32482a8bf9b327857a1211
RIJAYKUMAR RK <153521062+RijayKumarrk@users.noreply.github.com>
2025-03-18 11:34:23 +0530

    Add files via upload

---
+++ Arrays_insertion.java
@@ -0,0 +1,17 @@
+package bca;
+
+import java.util.Scanner;
+
+public class Arrays_insertion {
+	public static void main(String[] args) {
+		Scanner rk=new Scanner(System.in);
+        System.out.println("enter the value");
+        int arr=rk.nextInt();
+        int b = 0;
+		int [] a= new int[b];
+	}
+	
+	
+	
+
+}
